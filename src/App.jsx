27ce7384ffa5a5bdc393d035dfe60260a2578f6b
Minsung Kim <tkfpsk@gmail.com>
2025-06-03 22:04:44 +0100

    Stage 1: Set up the environment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./components/Hero";
+
+
+function App() {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <h1 className='text-5xl text-orange-500 font-bold'>lol</h1>
+      <Hero />
+    </main>
+  );
+}
+
+export default App;
