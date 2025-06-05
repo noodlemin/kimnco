20595dd46797c621012ac8b71964ee42c1b2262b
Minsung Kim <tkfpsk@gmail.com>
2025-06-05 09:48:25 +0100

    Stage 2: Hero done

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Hero from "./components/Hero";
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <h1 className='text-5xl text-orange-500 font-bold'>lol</h1>
       <Hero />
     </main>
   );
