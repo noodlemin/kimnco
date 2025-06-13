2504a9e117961bf52b7839a1bc7b700a4304265f
Minsung Kim <tkfpsk@gmail.com>
2025-06-13 23:05:54 +0100

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -86,7 +86,7 @@ const Features = () => {
   return(
     <section className="bg-black pb-52">
       <div className="container mx-auto px-3 md:px-10">
-        <div className="px-5 pt-32 pb-10">
+        <div className="px-5 pt-32 pb-5">
           <p className="font-circular-web text-2xl text-blue-50">
             {t('features.intro-title')}
           </p>
